dac0b27d89557cd8a0d84477e7c79caaf3f46518
JinUng <ingjwjw@naver.com>
2022-11-26 23:38:22 +0900

    상세 페이지 만들기

--- firebasetest/app/src/main/java/com/example/firebasetest/navigation/DetailViewFragment.kt
+++ firebasetest/app/src/main/java/com/example/firebasetest/navigation/DetailViewFragment.kt
@@ -5,15 +5,61 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.firebasetest.R
+import com.google.firebase.firestore.FirebaseFirestore
 
 class DetailViewFragment : Fragment() {
+    var fireStore : FirebaseFirestore? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail,container,false)
+        fireStore = FirebaseFirestore.getInstance()
         return view
+    }
+
+    inner class DetailViewRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+        var contentDTOs : ArrayList<ContentDTO> = arrayListOf()
+        var contentUidList : ArrayList<String> = arrayListOf()
+        init {
+
+            fireStore?.collection("images")?.orderBy("timestamp")?.addSnapshotListener { querySnapShot, firebaseFirestore ->
+                contentDTOs.clear()
+                contentUidList.clear()
+                for(snapshot in querySnapShot!!.documents){
+                    var item = snapshot.toObject(ContentDTO::class.java)
+                    contentDTOs.add(item!!)
+                    contentUidList.add(snapshot.id)
+                }
+                notifyDataSetChanged()
+            }
+
+
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            var view = LayoutInflater.from(parent.context).inflate(R.layout.item_detail,parent,false)
+            return CustomViewHolder(view)
+        }
+
+        inner class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            var viewHolder = (holder as CustomViewHolder).itemView
+
+            viewHolder.detailView_profile_textView.text = contentDTOs!![position].userId
+
+            Glide.with(holder.itemView.context).load(contentDTOs!![position].imageUrl).into(viewHolder.detailViewItem_imageView_content)
+        }
+
+        override fun getItemCount(): Int {
+            return contentDTOs.size
+        }
+
+
     }
 }
